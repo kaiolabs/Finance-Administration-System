b01d28ef708d0f7dfb95430ba94cc91859ae68b2
Arcanodev <peypro1@gmail.com>
2021-10-08 21:59:05 -0300

    master

---
+++ Projeto ADM/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+
+int main()
+{
+    setlocale(LC_ALL, "Portuguese");
+
+    printf("\n______________________________________\n\nSISTEMA DE ADIMINISTRAÇÀO DE EMPRESAS\n______________________________________\n");
+
+    return 0;
+}
