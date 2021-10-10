f96384eb3d300bd7498a2209728990229b9bf3d0
Arcanodev <peypro1@gmail.com>
2021-10-09 22:39:04 -0300

    att

--- Projeto ADM/main.c
+++ Projeto ADM/main.c
@@ -11,7 +11,7 @@ int main()
 
     printf("\n______________________________________________________\n\n\tSISTEMA DE ADIMINISTRAÇÀO DE EMPRESAS\n______________________________________________________\n");
 
-    printf("\n\n | (1) - Controle contas a receber.\n | (2) - Controle contas a pagar.\n | (3) - Gera relatórios financeiros.\n | (4) - Folha de pagamento dos funcionários\n | (5) - Lista de funcionários\n | (6) - Estoque de produtos\n | (7) - Fornecedores.\n |\n | INFORME O NÚMERO DA OPÇÃO DESEJADA: ");
+    printf("\n\n | (1) - Controle contas a receber.\n | (2) - Controle contas a pagar.\n | (3) - Gera relatórios financeiros.\n | (4) - Folha de pagamento dos funcionários.\n | (5) - Lista de funcionários.\n | (6) - Estoque de produtos.\n | (7) - Fornecedores.\n |\n | INFORME O NÚMERO DA OPÇÃO DESEJADA: ");
     scanf("%d", &opi);
 
     return 0;
