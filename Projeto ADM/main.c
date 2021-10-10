532f43b0e613bd746dade7f7dbf49fe3a2fc15bf
Arcanodev <peypro1@gmail.com>
2021-10-09 22:31:44 -0300

    ok

--- Projeto ADM/main.c
+++ Projeto ADM/main.c
@@ -7,7 +7,12 @@ int main()
 {
     setlocale(LC_ALL, "Portuguese");
 
-    printf("\n______________________________________\n\nSISTEMA DE ADIMINISTRAÇÀO DE EMPRESAS\n______________________________________\n");
+    int opi;
+
+    printf("\n______________________________________________________\n\n\tSISTEMA DE ADIMINISTRAÇÀO DE EMPRESAS\n______________________________________________________\n");
+
+    printf("\n\n | (1) - Controle contas a receber.\n | (2) - Controle contas a pagar.\n | (3) - Gera relatórios financeiros.\n | (4) - Folha de pagamento dos funcionários\n | (5) - Lista de funcionários\n | (6) - Estoque de produtos\n | (7) - Fornecedores.\n |\n | INFORME O NÚMERO DA OPÇÃO DESEJADA: ");
+    scanf("%d", &opi);
 
     return 0;
 }
