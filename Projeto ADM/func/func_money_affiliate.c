ca3d254e56a6f1f21cb62e668fa8178ee8fd3742
Arcanodev <92180339+Arcanodev@users.noreply.github.com>
2021-10-23 16:14:37 -0300

    NOVA ATUALIZAÇÃO

--- Projeto ADM/func/func_money_affiliate.c
+++ Projeto ADM/func/func_money_affiliate.c
@@ -2,81 +2,15 @@
 #include <stdlib.h>
 #include <locale.h>
 
-void Controle_receber ();
-void Controle_pagar ();
-void Gera_relatorio ();
-void Folha_de_pagamento();
-void Lista_de_funcionarios ();
-void Estoque_de_produtos ();
-void Fornecedores ();
-
 int main()
 {
     setlocale(LC_ALL, "Portuguese");
-
-    int opi;
-
-    do{
-    
-    printf("\n-------------------------------------------------------\n\tSISTEMA DE ADIMINISTRAÇÀO DE EMPRESAS\n-------------------------------------------------------\n");
-
-    printf("\n\n | (1) - Controle de dinheiro a recebe.\n | (2) - Controle de contas a paga.\n | (3) - Gera relatórios financeiros.\n | (4) - Folha de pagamento dos funcionários.\n | (5) - Lista de funcionários.\n | (6) - Estoque de produtos.\n | (7) - Fornecedores.\n |\n | INFORME O NÚMERO DA OPÇÃO DESEJADA: ");
-    scanf("%d", &opi);
-
-
-    if(opi == 1){
-
-        Controle_receber ();
-
-    }
-        else if(opi == 2){
-            
-            Controle_pagar ();
-
-        }
-            else if (opi == 3){
-
-                Gera_relatorio ();
-
-            }
-                else if (opi == 4){
-
-                    Folha_de_pagamento ();
-
-                }
-                    else if (opi == 5){
-
-                        Lista_de_funcionarios ();
-
-                    }
-                        else if (opi == 6){
-
-                            Estoque_de_produtos ();
-
-                        }
-                            else if(opi == 7){
-
-                                Fornecedores ();
-
-                            }
-
-    }while (opi != 0);
-    
-
-    return 0;
-}
-
-void Controle_receber (){
-
-}
-
-void Controle_pagar (){
 
     float money1, money2, money3, money4;
     static int opi, soma1 = 0, soma2 = 0, soma3 = 0 , soma4 = 0;
     static float soma5 = 0;
 
-    printf("\n-----------------------------------------------\n\tCONTROLE DE CONTAS A PAGA\n-----------------------------------------------\n");
+    printf("\n-----------------------------------------------\n\tControle de contas a pagar\n-----------------------------------------------\n");
 
     do{
 
@@ -129,24 +63,5 @@ void Controle_pagar (){
 
     }while(opi != 0);
 
-}
-
-void Gera_relatorio (){
-
-}
-
-void Folha_de_pagamento(){
-
-}
-
-void Lista_de_funcionarios (){
-
-}
-
-void Estoque_de_produtos (){
-
-}
-
-void Fornecedores (){
-
+    return 0;
 }
